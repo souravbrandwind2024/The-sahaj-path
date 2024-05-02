381a3faf61f079f47b27f47d765a960928c504f9
souravbrandwind2024 <sourav@brandwind.in>
2024-05-02 12:29:04 +0530

    header sticky done

--- js/main.js
+++ js/main.js
@@ -169,3 +169,28 @@ $( document ).ready(function() {
     $(".number").counterUp({time:5000});
 });
 // total-visitors-counter-area-end //
+
+
+
+function stickyHeader() {
+    if ($('.stricky').length) {
+        var strickyScrollPos = 100;
+        if ($(window).scrollTop() > strickyScrollPos) {
+            $('.stricky').removeClass('slideIn animated');
+            $('.stricky').addClass('stricky-fixed slideInDown animated');
+            $('.scroll-to-top').fadeIn(500);
+        } else if ($(this).scrollTop() <= strickyScrollPos) {
+            $('.stricky').removeClass('stricky-fixed slideInDown animated');
+            $('.stricky').addClass('slideIn animated');
+            $('.scroll-to-top').fadeOut(500);
+        }
+    };
+}
+
+
+// instance of fuction while Window Scroll event
+jQuery(window).on('scroll', function() {
+    (function($) {
+        stickyHeader();     
+    })(jQuery);
+});
